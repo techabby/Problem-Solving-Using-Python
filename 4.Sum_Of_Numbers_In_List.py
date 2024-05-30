b5c41d80ab823f3dd17a1b2ac32646c96642f234
Abdullah Aziz <abbyheere@gmail.com>
2024-05-31 04:54:48 +0500

    Sum Of Numbers In A List

---
+++ 4.Sum_Of_Numbers_In_List.py
@@ -0,0 +1,15 @@
+# Write a function to sum all the numbers in a list.
+
+i = int(input("Enter number of numbers you want to add in the list: \n"))
+list = []
+
+for j in range(i):
+    no = int(input("Emter a number: "))
+    list.append(no)
+
+sum = 0
+
+for num in list:
+    sum = sum + num
+
+print(f"\nThe sum of {i} numbers is {sum}.")
