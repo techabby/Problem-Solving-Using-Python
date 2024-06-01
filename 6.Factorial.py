92b6aad6bd6adcc63d8242387ba361289adae9d2
Abdullah Aziz <abbyheere@gmail.com>
2024-06-02 00:41:48 +0500

    Factorial Of A Number

---
+++ 6.Factorial.py
@@ -0,0 +1,10 @@
+# Write a function to find the factorial of a number.
+
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(num, 0, -1):
+    factorial = factorial * i
+
+print(f"\nThe factorial of {num} is {factorial}.")
