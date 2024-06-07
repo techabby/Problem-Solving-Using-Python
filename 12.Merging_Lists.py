376a473b48edb1809387a6fe92a2e8251163e315
Abdullah Aziz <abbyheere@gmail.com>
2024-06-08 03:37:46 +0500

    Merging Sorted Lists

---
+++ 12.Merging_Lists.py
@@ -0,0 +1,12 @@
+# Write a program to merge two sorted lists.
+
+list1 = [5,23,2,131,21,32,4]
+list2 = [0,234,32,7,84,989]
+
+list1.sort()
+list2.sort()
+
+merged_list = list1 + list2
+
+print("The merged list is:",merged_list)
+
